02f2de182b8c125d0c637fd1a191e9151d5f772e
Aom Siriwat <siriwat576@gmail.com>
2024-02-11 03:34:31 +0700

    feat: Add CORS support to NestFactory.create

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,6 +5,7 @@ import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.enableCors();
 
 	app.enableVersioning({
 		type: VersioningType.URI,
