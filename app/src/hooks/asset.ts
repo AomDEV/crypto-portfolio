9482c3445582a55ffdd23151420cf78c814db226
Aom Siriwat <siriwat576@gmail.com>
2024-02-11 04:30:51 +0700

    feat: Add useAsset hook for asset listing

---
+++ app/src/hooks/asset.ts
@@ -0,0 +1,50 @@
+import { api } from "@/lib/api";
+import { PaginationMeta } from "@/types/pagination";
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+type UseAssetOptions = {
+    page?: number;
+    limit?: number;
+    callOnMount?: boolean;
+};
+export function useAsset (options: UseAssetOptions = {}) {
+    options = Object.assign({
+        page: 1,
+        limit: 10,
+        callOnMount: true,
+    }, options);
+    const { page: _page, limit, callOnMount } = options;
+    
+    const [data, setData] = useState<any[]>([]);
+    const [meta, setMeta] = useState<PaginationMeta | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const noData = useMemo(() => !isLoading && data.length === 0, [data, isLoading]);
+
+    const fetch = useCallback(async (page: number) => {
+        setIsLoading(true)
+        setMeta(null);
+        return api().get(`/v1/asset/listing`, {
+            params: {
+                page,
+                limit,
+            }
+        }).then((response) => {
+            const { data, meta } = response.data;
+            setData(data);
+            setMeta(meta);
+        }).finally(() => {
+            setIsLoading(false);
+        })
+    }, [_page, limit]);
+    useEffect(() => {
+        if (callOnMount) fetch(_page ?? 1);
+    }, [_page, limit]);
+
+    return {
+        data,
+        meta,
+        isLoading,
+        noData,
+        fetch,
+    };
+}
