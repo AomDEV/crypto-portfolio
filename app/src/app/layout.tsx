f34feffda50b1ede7cba372660243904ab68d305
Aom Siriwat <siriwat576@gmail.com>
2024-02-12 02:16:22 +0700

    feat: Add Toaster component to RootLayout

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SseProvider } from "@/context/sse";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
 				<SseProvider>
 					{children}
 				</SseProvider>
+				<Toaster />
 			</body>
 		</html>
 	);
