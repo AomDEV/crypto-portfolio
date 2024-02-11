f2e3d02f0078852a4a9ecc360adc94f6379d373f
Aom Siriwat <siriwat576@gmail.com>
2024-02-11 22:11:36 +0700

    fix: Add request close event listener in SSE endpoint

--- api/src/features/event/event.controller.ts
+++ api/src/features/event/event.controller.ts
@@ -1,10 +1,11 @@
-import { BadRequestException, Controller, Query, Sse } from "@nestjs/common";
+import { BadRequestException, Controller, Logger, Query, Req, Sse } from "@nestjs/common";
 import { ApiQuery, ApiTags } from "@nestjs/swagger";
 import { EventService } from "./event.service";
 import { Guest } from "@/common/decorators/guest";
 import { Observable } from "rxjs";
 import { EVENT } from "./constant";
 import { SubscribeUsecase } from "./usecases/subscribe.usecase";
+import { Request } from "express";
 
 @Controller({path: 'event', version: '1'})
 @ApiTags('Event')
@@ -17,8 +18,13 @@ export class EventController {
     @Guest()
     @ApiQuery({name: 'event', required: true, type: String, description: 'Event name to subscribe'})
     async sse (
-        @Query('event') event?: string
+        @Req() request: Request,
+        @Query('event') event?: string,
     ): Promise<Observable<MessageEvent>> {
+        request.on('close', (e) => {
+            new Logger().warn('Connection closed', e);
+        });
+        
         return this.subscribeUsecase.execute({event});
     }
 }
