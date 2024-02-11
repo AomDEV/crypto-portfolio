539bccba7bdf291012f0fc352598a2cf26e64c4b
Aom Siriwat <siriwat576@gmail.com>
2024-02-12 03:29:21 +0700

    feat: Add new page component for authentication check

---
+++ app/src/app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { LoadingText } from "@/components/loading";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Page () {
+    const router = useRouter();
+    useEffect(() => {
+        // check `access_token` from localStorage if not exists redirect to login if not then redirect to `portfolio`
+        const accessToken = localStorage.getItem('access_token');
+        if (!accessToken) return router.push('/login');
+        return router.push('/portfolio');
+    }, []);
+    return <LoadingText />
+}
