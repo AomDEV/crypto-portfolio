61243450298b48a8bf7c2641790d812c01dc9c57
Aom Siriwat <siriwat576@gmail.com>
2024-02-12 03:27:35 +0700

    feat: Add member sign-out functionality

--- app/src/app/(member)/layout.tsx
+++ app/src/app/(member)/layout.tsx
@@ -1,10 +1,22 @@
+"use client";
 import Nav from "@/components/nav";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function MemberLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const router = useRouter();
+    useEffect(() => {
+        // check `access_token` from localStorage if not exists redirect to login
+        const accessToken = localStorage.getItem('access_token');
+        if (!accessToken) {
+            localStorage.clear();
+            return router.push('/login');
+        }
+    }, []);
     return (
         <main className="min-h-screen flex flex-col justify-center items-center">
             <div className="w-full max-w-xl">
---
+++ app/src/app/(member)/sign-out/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { LoadingText } from "@/components/loading";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Page () {
+    const router = useRouter();
+    useEffect(() => {
+        localStorage.clear();
+        return router.push('/login');
+    }, []);
+    return <LoadingText />
+}
